b7a6a2eb124c9a6c6c3e8c8b5ab4d073f82b819e
Mirze26 <mirzaab@code.edu.az>
2024-04-30 21:46:56 +0400

    user-info

---
+++ promt with user about/src/js/script.js
@@ -0,0 +1,19 @@
+
+var namee = prompt("Name");
+var surname = prompt("Surname");
+var birthyear = Number(prompt("Birthyear"));
+var age = 2024 - birthyear;
+
+alert(namee +" "+   surname +" "+  age);
+
+console.log(namee);
+console.log(surname);
+console.log(age);
+
+
+
+
+
+
+
+
